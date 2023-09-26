5f293e461130f289b85e7b21d7204d24e0f612e4
Festus <festusasiyanbi80@gmail.com>
2023-09-26 10:01:18 -0400

    island parameter algorithm

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+Island Perimeter:
+    returns the perimeter of the island described in grid
+"""
+
 def island_perimeter(grid):
     if not grid:
         return 0
@@ -21,3 +27,19 @@ def island_perimeter(grid):
                     perimeter -= 1
 
     return perimeter
+
+
+
+
+# def island_perimeter(grid):
+#     """island perimenter function"""
+#     perimeter = 0
+#     for i in range(len(grid)):
+#         for j in range(len(grid[i])):
+#             if grid[i][j] == 1:
+#                 perimeter += 4
+#                 if i > 0 and grid[i-1][j] == 1:
+#                     perimeter -= 2
+#                 if j > 0 and grid[i][j-1] == 1:
+#                     perimeter -= 2
+#     return perimeter
