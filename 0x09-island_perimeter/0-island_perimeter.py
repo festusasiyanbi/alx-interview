c9a285a1094c90cf289ed2acc8b5ae897f85aecb
Festus <festusasiyanbi80@gmail.com>
2023-09-26 10:15:29 -0400

    corrected an error

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -15,7 +15,7 @@ def island_perimeter(grid):
     cols = len(grid[0])
 
     for row in range(rows):
-        for col in range(cols[row]):
+        for col in range(cols):
             if grid[row][col] == 1:
                 perimeter += 4
                 if row > 0 and grid[row-1][col] == 1:
