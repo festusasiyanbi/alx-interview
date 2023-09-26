8c2058d4733baee097862977b75ae0ed3582df4d
Festus <festusasiyanbi80@gmail.com>
2023-09-26 10:03:04 -0400

    island parameter algorithm

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -18,13 +18,13 @@ def island_perimeter(grid):
 
                 # Check adjacent cells (up, down, left, and right)
                 if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 1
-                if row < rows - 1 and grid[row + 1][col] == 1:
-                    perimeter -= 1
+                    perimeter -= 2
+                # if row < rows - 1 and grid[row + 1][col] == 1:
+                #     perimeter -= 1
                 if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 1
-                if col < cols - 1 and grid[row][col + 1] == 1:
-                    perimeter -= 1
+                    perimeter -= 2
+                # if col < cols - 1 and grid[row][col + 1] == 1:
+                #     perimeter -= 1
 
     return perimeter
 
