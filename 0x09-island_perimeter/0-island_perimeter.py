3b677d623c3fd524357d4a9f2b5f39444acdcb79
Festus <festusasiyanbi80@gmail.com>
2023-09-26 10:08:29 -0400

    island parameter algorithm

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,42 +4,43 @@ Island Perimeter:
     returns the perimeter of the island described in grid
 """
 
+
 def island_perimeter(grid):
+    """island perimenter function"""
     if not grid:
         return 0
-
     perimeter = 0
-    rows, cols = len(grid), len(grid[0])
-
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                perimeter += 4  # Start with 4 sides for each land cell
-
-                # Check adjacent cells (up, down, left, and right)
-                if row > 0 and grid[row - 1][col] == 1:
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
                     perimeter -= 2
-                # if row < rows - 1 and grid[row + 1][col] == 1:
-                #     perimeter -= 1
-                if col > 0 and grid[row][col - 1] == 1:
+                if j > 0 and grid[i][j-1] == 1:
                     perimeter -= 2
-                # if col < cols - 1 and grid[row][col + 1] == 1:
-                #     perimeter -= 1
-
     return perimeter
 
 
-
-
 # def island_perimeter(grid):
-#     """island perimenter function"""
+#     if not grid:
+#         return 0
+
 #     perimeter = 0
-#     for i in range(len(grid)):
-#         for j in range(len(grid[i])):
-#             if grid[i][j] == 1:
-#                 perimeter += 4
-#                 if i > 0 and grid[i-1][j] == 1:
-#                     perimeter -= 2
-#                 if j > 0 and grid[i][j-1] == 1:
-#                     perimeter -= 2
-#     return perimeter
+    # rows, cols = len(grid), len(grid[0])
+
+    # for row in range(rows):
+    #     for col in range(cols):
+    #         if grid[row][col] == 1:
+    #             perimeter += 4  # Start with 4 sides for each land cell
+
+    #             # Check adjacent cells (up, down, left, and right)
+    #             if row > 0 and grid[row - 1][col] == 1:
+    #                 perimeter -= 2
+    #             # if row < rows - 1 and grid[row + 1][col] == 1:
+    #             #     perimeter -= 1
+    #             if col > 0 and grid[row][col - 1] == 1:
+    #                 perimeter -= 2
+    #             # if col < cols - 1 and grid[row][col + 1] == 1:
+    #             #     perimeter -= 1
+
+    # return perimeter
